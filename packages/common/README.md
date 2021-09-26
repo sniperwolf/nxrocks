160adeadbf883f94cfcacfa0ef85d9df5d3e793f
Tine Kondo <kondotine@gmail.com>
2021-09-26 05:37:57 +0200

    refactor(common): add a common package to share code among plugins

---
+++ packages/common/README.md
@@ -0,0 +1,8 @@
+# common
+
+Internal library to shared code among our plugins
+
+
+## License
+
+Copyright (c) 2020-2021 Tine Kondo. Licensed under the MIT License (MIT)
---
+++ packages/common/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/core/jvm';
+export * from './lib/core/workspace';
+export * from './lib/core/constants';
+export * from './lib/core/utils';
+
+export * from './lib/testing';
